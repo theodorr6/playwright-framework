bcb2cc6ee7bd6eee4b0b49877adba3d28e155993
Teodor Virjoghe <imhunterize@gmail.com>
2025-08-01 14:21:37 +0300

    remove config.py from git

--- config.py
+++
@@ -1,8 +0,0 @@
-class BrowserConfig:
-    """Browser configuration settings"""
-    BROWSER = "firefox"
-    HEADLESS = False
-    SLOW_MO = 500
-
-    VIEWPORT_WIDTH = 1920
-    VIEWPORT_HEIGHT = 1080
